f8f90b234dca417f886be5f3d9698bcdcc682579
chanjung Kye <chanjung4505@gmail.com>
2023-03-09 19:48:40 +0900

    refactoring GradeCalculator to be first-class collection by making Courses.java class.

--- src/main/java/org/example/GradeCalculator.java
+++ src/main/java/org/example/GradeCalculator.java
@@ -3,22 +3,19 @@ package org.example;
 import java.util.List;
 
 public class GradeCalculator {
-    private final List<Course> courses;
+    private final Courses courses;
+
 
     public GradeCalculator(List<Course> courses) {
-        this.courses = courses;
+        this.courses = new Courses(courses);
     }
 
     public double calculate() {
-        double multipliedCreditAndCourseGrade = 0;
-        for (Course course : courses) {
-            multipliedCreditAndCourseGrade += course.multiplyCreditAndCourseGrade();
-        }
 
-        int totalCompletedCredit = courses.stream()
-                .mapToInt(Course::getCredit)
-                .sum();
+        double totalMultipliedCreditAndCourseGrade = courses.multiplyCreditAndCourseGrade();
 
-        return multipliedCreditAndCourseGrade / totalCompletedCredit;
+        int totalCompletedCredit = courses.calculateTotalCompletedCredit();
+
+        return totalMultipliedCreditAndCourseGrade / totalCompletedCredit;
     }
 }
